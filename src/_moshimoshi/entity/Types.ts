17eee378e5726604fe786c651808038810f0f4f0
Fernando Salom <fer@rudo.es>
2024-11-02 03:17:54 +0100

    Add types for endpoint structure

---
+++ src/_moshimoshi/entity/Types.ts
@@ -0,0 +1,15 @@
+export enum HTTPMethod {
+    GET = "GET",
+    POST = "POST",
+    PATCH = "PATCH",
+    PUT = "PUT",
+    DELETE = "DELETE",
+}
+
+export enum ContentType {
+    JSON = "application/json",
+    XML = "text/xml; charset=utf-8",
+    IMAGE = "image/jpeg",
+    FORM = "application/x-www-form-urlencoded",
+    NONE = "",
+}
