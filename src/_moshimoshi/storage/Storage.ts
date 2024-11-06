691ade89c7ce9dc857d131c4c05ea3dd4a6d0b5a
Fernando Salom <fer@rudo.es>
2024-11-06 02:41:32 +0100

    Fix interface for storage

--- src/_moshimoshi/storage/Storage.ts
+++ src/_moshimoshi/storage/Storage.ts
@@ -2,7 +2,7 @@ import {TokenType} from "./TokenType";
 import {Token} from "../entity/Token";
 
 export interface Storage {
-    save(tokens: Token, type: TokenType): void;
+    save(token: Token, type: TokenType): void;
     retrieve(type: TokenType): Token | null;
     deleteAll(): void;
 }
