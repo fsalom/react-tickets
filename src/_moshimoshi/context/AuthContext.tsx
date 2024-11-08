a9389bc22dae829082304591b558652133142296
Fernando Salom <fer@rudo.es>
2024-11-08 08:34:41 +0100

    Replace part of the functionality and add moshi as responsible

--- src/_moshimoshi/context/AuthContext.tsx
+++ src/_moshimoshi/context/AuthContext.tsx
@@ -1,67 +1,39 @@
-// AuthContext.tsx
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { Tokens } from '../entity/Tokens';
-import { Token } from '../entity/Token';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import {Moshimoshi} from "../Moshimoshi";
 
 interface AuthContextType {
-    tokens: Tokens | null;
     isAuthenticated: boolean;
     loading: boolean;
     checkAuthStatus: () => void;
-    logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [tokens, setTokens] = useState<Tokens | null>(null);
+interface AuthProviderProps {
+    children: React.ReactNode;
+    authenticator: Moshimoshi;
+}
+
+export const AuthProvider: ({children, authenticator}: AuthProviderProps) => React.JSX.Element = ({
+                                                    children,
+                                                    authenticator,
+                                                }: AuthProviderProps) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-    const [loading, setLoading] = useState<boolean>(true); // Estado de carga
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        const accessTokenValue = localStorage.getItem('accessToken');
-        const refreshTokenValue = localStorage.getItem('refreshToken');
-
-        if (accessTokenValue && refreshTokenValue) {
-            const accessToken = new Token(accessTokenValue, 3600); // Ajusta la expiración
-            const refreshToken = new Token(refreshTokenValue, 86400);
-            const tokensInstance = new Tokens(accessToken, refreshToken);
-
-            setTokens(tokensInstance);
-            setIsAuthenticated(tokensInstance.isAccessTokenValid || tokensInstance.isRefreshTokenValid);
-        }
-
-        setLoading(false); // Estado de carga finalizado
+        authenticator.getAccessToken();
+        setIsAuthenticated(true)
+        setLoading(false);
     }, []);
 
     const checkAuthStatus = () => {
-        const accessTokenValue = localStorage.getItem('accessToken');
-        const refreshTokenValue = localStorage.getItem('refreshToken');
-        if (accessTokenValue && refreshTokenValue) {
-            const accessToken = new Token(accessTokenValue, 3600); // Ajusta la expiración
-            const refreshToken = new Token(refreshTokenValue, 86400);
-            const tokens = new Tokens(accessToken, refreshToken);
-        }
-
-        if (tokens) {
-            const valid = tokens.isAccessTokenValid || tokens.isRefreshTokenValid;
-            setIsAuthenticated(valid);
-
-            if (!valid) {
-                logout();
-            }
-        }
-    };
-
-    const logout = () => {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        setTokens(null);
-        setIsAuthenticated(false);
+        const accessTokenValue = authenticator.getAccessToken()
+        setIsAuthenticated(true);
     };
 
     return (
-        <AuthContext.Provider value={{ tokens, isAuthenticated, loading, checkAuthStatus, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, loading, checkAuthStatus }}>
             {children}
         </AuthContext.Provider>
     );
