76759f6045d02c1cc73269cbf88174b6c77fccc8
Fernando Salom <fer@rudo.es>
2024-11-09 09:44:28 +0100

    Fix authentication provider with DI for moshi moshi

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,22 +1,29 @@
 import './App.css';
 import '@fontsource/roboto';
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import AppTheme from "./theme/AppTheme";
 import SignInSideView from "./pages/sign-in-side/View";
 import {AuthProvider} from "../_moshimoshi/context/AuthContext";
 import ProtectedRoute from '../_moshimoshi/route/ProtectedRoute';
 import HomeView from "./pages/home/View";
+import SignUpView from "./pages/sign-up/View";
+import {Home} from "@mui/icons-material";
+import AppTheme from "./theme/AppTheme";
+import {Config} from "../config";
 
 
 function App() {
   return (
-      <AuthProvider>
+      <AuthProvider authenticator={Config.getInstance().moshimoshi}>
           <AppTheme>
               <BrowserRouter>
                   <Routes>
                       <Route
                           path="/login"
                           element={<SignInSideView />}
+                      />
+                      <Route
+                          path="/sign-up"
+                          element={<SignUpView />}
                       />
                       <Route
                           path="/home"
